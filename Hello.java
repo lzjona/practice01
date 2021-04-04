d97c371d91c6904467804e635e1bf53b52867916
Jonathan Lee <lzjonadev@gmail.com>
2021-04-05 02:20:22 +0800

    Docker message updated

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class Hello {
 
     public static void main(String[] args) {
 
-      System.out.println("This is a java app created using Docker");
+      System.out.println("This is a java app created using Docker with trigger");
 
     }
 
